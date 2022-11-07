106f08e94d70041ee008b811b9dc59feeccaebbf
yanxiaoqi932 <pwq93208@163.com>
2022-11-08 01:04:29 +0800

    count fig

--- runner.py
+++ runner.py
@@ -3,11 +3,30 @@ from typing import Dict, List
 import scheduler
 from math import ceil
 import json
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.pyplot import MultipleLocator
 
-# worst score: -172600.5
-# now score: -609.0
-# now score: -541.0
-# now score: -476.0
+plt.rcParams['font.sans-serif']=['SimSun'] # 用来正常显示中文标签
+
+count: Dict[str, Dict[int, int]] = {}
+count['FE']:Dict[int, int] = {}
+count['BE']:Dict[int, int] = {}
+count['EM']:Dict[int, int] = {}
+for i in range(-12, 12+1):
+    count["FE"][i] = 0
+    count["BE"][i] = 0
+    count["EM"][i] = 0
+
+deduct: Dict[str, Dict[int, int]] = {}
+deduct['FE']:Dict[int, int] = {}
+deduct['BE']:Dict[int, int] = {}
+deduct['EM']:Dict[int, int] = {}
+for i in range(-12, 12+1):
+    deduct["FE"][i] = 0
+    deduct["BE"][i] = 0
+    deduct["EM"][i] = 0
+
 class Runner:
     def __init__(self, filename, debug=True) -> None:  
         """init runner object
@@ -59,7 +78,7 @@ class Runner:
         self.clock += 1
         return
     
-    def judge(self) -> float:
+    def judge(self):
         """score a scheduler
 
         Returns:
@@ -82,7 +101,7 @@ class Runner:
                 requests_json = [json.dumps(x) for x in self.hour_reqs]
                 drivers_json = [json.dumps(x) for x in self.drivers]
                 start_time = perf_counter()
-                scheduled_json = self.scheduler.schedule(self.clock, requests_json, drivers_json)
+                scheduled_json = self.scheduler.schedule(self.clock - 1, requests_json, drivers_json)
                 duration = perf_counter() - start_time
                 scheduled = [json.loads(x) for x in scheduled_json]
                 self.total_ref_time += duration
@@ -103,20 +122,29 @@ class Runner:
                         if driver_id not in req_driver:
                             if self.debug:
                                 print(f"DriverID={driver_id} not in {req_driver}")
+                                raise AttributeError('driver_id not in req_driver')
                             self.score -= 24 * ceil(req_size / 50)
                         else:
                             if capacity >= req_size:
                                 self.requests[req]["Done"] = True
                                 capacity -= req_size
+                                now_sla = req_sla - (self.clock - req_clock)
+                                if now_sla < -12:
+                                    now_sla = -12
+                                count[req_type][now_sla] += 1
                                 if req_type == "FE":
                                     if self.clock - req_clock > req_sla:
                                         self.score -= min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
+                                        deduct["FE"][now_sla] += min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
                                 elif req_type == "BE":
                                     if self.clock - req_clock <= req_sla:
                                         self.score += 0.5 * ceil(req_size / 50)
+                                    else:
+                                        deduct["BE"][now_sla] += 0.5 * ceil(req_size / 50)
                                 else: # EM
                                     if self.clock - req_clock > req_sla:
                                         self.score -= 2 * min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
+                                        deduct["EM"][now_sla] += 2 * min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
                             else:
                                 if self.debug:
                                     print(f"DriverID={driver_id} has a size of {req_size} lager than capacity remained")
@@ -131,21 +159,83 @@ class Runner:
                         req_clock = req["LogicalClock"]
                         req_sla = req["SLA"]
                         if not req["Done"]:
+                            now_sla = req_sla - (self.clock - req_clock)
+                            if now_sla < -12:
+                                now_sla = -12
+                            count[req_type][now_sla] += 1
                             if req_type == "FE":
                                 if self.clock - req_clock > req_sla:
                                     self.score -= min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
+                                    deduct["FE"][now_sla] += min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
                             elif req_type == "EM":
                                 if self.clock - req_clock > req_sla:
                                     self.score -= 2 * min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
-                            else: # BE
+                                    deduct["EM"][now_sla] += 2 * min(12, self.clock - req_clock - req_sla) * ceil(req_size / 50)
+                            elif req_type == "BE": # BE
+                                if self.clock - req_clock > req_sla:
+                                    deduct["BE"][now_sla] += 0.5 * ceil(req_size / 50)
+                            else:
                                 pass
                     print(f"Total reference time:{self.total_ref_time:.6f}s")               
                     print(f"Memory occupied every schedule(MB): {self.scheduler.memory}")
-                return self.score
+                print(f'********count********')
+                print(count)
+                print(deduct)
+                return self.score, count, deduct
+
+def fig(count: Dict[str, Dict[int, int]], ylabel:str) -> None:
+    fe_count: List[int] = []
+    be_count: List[int] = []
+    em_count: List[int] = []
+
+    for i in range(-12, 12+1):
+        fe_count.append(count['FE'][i])
+        be_count.append(count['BE'][i])
+        em_count.append(count['EM'][i])
+
+    fig = plt.figure(figsize=(10, 6))
+
+    ax1 = fig.add_subplot(311)
+    ax1.bar(list(range(-12, 12+1)), fe_count)
+    ax1.set_title('FE')
+    x_major_locator = MultipleLocator(1.0)  # 设置刻度间隔
+    ax1.xaxis.set_major_locator(x_major_locator)
+    plt.xlabel('提交时剩余时间')
+    plt.ylabel(ylabel)
+    for a,b in zip(np.arange(-12, 12+1), fe_count):   #柱子上的数字显示
+        plt.text(a,b,'%.d'%(b),ha='center',va='bottom',fontsize=10)
+
+    ax2 = fig.add_subplot(312)
+    ax2.bar(list(range(-12, 12+1)), be_count)
+    ax2.set_title('BE')
+    x_major_locator = MultipleLocator(1.0)  # 设置刻度间隔
+    ax2.xaxis.set_major_locator(x_major_locator)
+    plt.xlabel('提交时剩余时间')
+    plt.ylabel(ylabel)
+    for a,b in zip(np.arange(-12, 12+1), be_count):   #柱子上的数字显示
+        plt.text(a,b,'%.d'%(b),ha='center',va='bottom',fontsize=10)
+
+    ax3 = fig.add_subplot(313)
+    ax3.bar(list(range(-12, 12+1)), em_count)
+    ax3.set_title('EM')
+    x_major_locator = MultipleLocator(1.0)  # 设置刻度间隔
+    ax3.xaxis.set_major_locator(x_major_locator)
+    plt.xlabel('提交时剩余时间')
+    plt.ylabel(ylabel)
+    for a,b in zip(np.arange(-12, 12+1), em_count):   #柱子上的数字显示
+        plt.text(a,b,'%.d'%(b),ha='center',va='bottom',fontsize=10)
+
+    fig.subplots_adjust(hspace=1.0)
+
+    plt.show()
 
 r = Runner("demo.log")
-score = r.judge()
+score, count, deduct = r.judge()
 print(score)
+fig(count, '提交数量')
+fig(deduct, '扣分')
+
+
         
         
         
